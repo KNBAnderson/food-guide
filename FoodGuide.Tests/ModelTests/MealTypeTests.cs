c3326ce8561ed68c8fce516803e5537a4ecefba0
Justin Kerntz and Katlin Anderson <justin@>
2019-05-08 16:41:15 -0700

    create initial files and MVC structure. Add html to home page.

---
+++ FoodGuide.Tests/ModelTests/MealTypeTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodGuide.Models;
+using System.Collections.Generic;
+using System;
+
+namespace FoodGuide.Tests
+{
+  [TestClass]
+  public class MealTypeTest : IDisposable
+  {
+
+  }
+  
+}
